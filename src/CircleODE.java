756affb4a6ed90e825ac2e58264194d228b7263f
Tom Boyd <tboyd@scitec.com>
2016-03-25 19:51:29 -0400

    added Jacobian secondary state to CircleODE

--- src/CircleODE.java
+++ src/CircleODE.java
@@ -1,7 +1,13 @@
-import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
-import org.apache.commons.math3.ode.nonstiff.DormandPrince853Integrator;
+import org.apache.commons.math3.ode.*;
+import org.apache.commons.math3.ode.nonstiff.DormandPrince853Integrator;;
+import java.util.*;
 
-public class CircleODE implements FirstOrderDifferentialEquations {
+
+public class CircleODE implements MainStateJacobianProvider{
+
+    public static final String CENTER_X = "cx";
+    public static final String CENTER_Y = "cy";
+    public static final String OMEGA    = "omega";
 
     private double[] c;
     private double omega;
@@ -16,20 +22,53 @@ public class CircleODE implements FirstOrderDifferentialEquations {
     }
 
     public void computeDerivatives(double t, double[] y, double[] yDot) {
+        // the state is a 2D point, the ODE therefore corresponds to the
+        // velocity
         yDot[0] = omega * (c[1] - y[1]);
         yDot[1] = omega * (y[0] - c[0]);
     }
 
-	public static void main(String[] args){
-		DormandPrince853Integrator dp853 = new DormandPrince853Integrator(
-			1.0e-8, 100.0, 1.0e-10, 1.0e-10);
-		CircleODE ode = new CircleODE(new double[] {1,1}, 0.1);
+    public void computeMainStateJacobian(
+    double t, double[] y, double[] yDot, double[][] dFdY) {
 
-		double[] y = new double[] { 0.0, 1.0 }; // initial state
+        // compute the Jacobian of the main state
+        dFdY[0][0] = 0;
+        dFdY[0][1] = -omega;
+        dFdY[1][0] = omega;
+        dFdY[1][1] = 0;
+    }
 
-		for (double t=0; t < 1<<15; t+=0.3){
-			dp853.integrate(ode, t, y, t+0.3, y);
-			System.out.printf("t = %.3f\ty = [%.3f,%.3f]\n",t,y[0],y[1]);
-		}
-	}
+     public static void main(String[] args){
+
+        CircleODE circle = new CircleODE(new double[] {1.0,  1.0 }, 0.1);
+
+        // here, we could select only a subset of the parameters, or use
+        // another order
+        JacobianMatrices jm = new JacobianMatrices(circle, CircleODE.CENTER_X,
+        CircleODE.CENTER_Y, CircleODE.OMEGA);
+
+        ExpandableStatefulODE efode = new ExpandableStatefulODE(circle);
+        efode.setTime(0);
+        double[] y = { 1.0, 0.0 };
+        efode.setPrimaryState(y);
+
+        // create the variational equations and append them to the main
+        // equations, as secondary equations
+        jm.registerVariationalEquations(efode);
+
+        // integrate the compound state, with both main and additional
+        // equations
+        DormandPrince853Integrator integrator = new DormandPrince853Integrator(
+            1.0e-6, 1.0e3, 1.0e-10, 1.0e-12);
+        integrator.setMaxEvaluations(5000);
+        integrator.integrate(efode, 20.0);
+
+        // retrieve the Jacobian of the final state with respect to initial
+        // state
+        double[][] dYdY0 = new double[2][2];
+        jm.getCurrentMainSetJacobian(dYdY0);
+
+    }
+        
+
 }
